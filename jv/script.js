48b82baecacbe2432d775c9645ce730582d5eae6
94tony <dagatatony94@gmail.com>
2021-05-27 14:59:12 +0200

    sercizio

---
+++ jv/script.js
@@ -0,0 +1,6 @@
+alert("Generatore di password") ;
+var nome = prompt("nome?") ;
+var cognome = prompt("cognome?") ;
+var colore = prompt("colore preferito?") ;
+var a = "21" ;
+document.getElementById('risultato').innerHTML= nome + cognome + colore + a;
